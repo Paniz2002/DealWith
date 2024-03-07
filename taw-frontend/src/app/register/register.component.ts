5e93fe37021bf1893d7bb7203b7ad4c6646ef5f0
Tommaso <soncintommaso@gmail.com>
2024-03-07 11:49:39 +0100

    working on form request

--- taw-frontend/src/app/register/register.component.ts
+++ taw-frontend/src/app/register/register.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import axios from 'axios';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  // Bypassing compiler error with this simple trick
+  // instead of changing tsconfig.json
+  // 99.9% of companies will hate you, but
+  // we know that this registerForm WILL get initialized
+  // from the constructor.
+  registerForm!: FormGroup;
+  constructor(private registerFormBuilder: FormBuilder) {
+    this.createRegisterForm();
+  }
 
+  createRegisterForm() {
+    this.registerForm = this.registerFormBuilder.group({
+      name: [''],
+      email: [''],
+      password: [''],
+      confirmPassword: [''],
+      role: [''],
+    });
+  }
+  onSubmit() {
+    console.log(this.registerForm.get('name')?.value);
+  }
 }
