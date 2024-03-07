985dc0dffb75ea9dd0c2e91a002b3a851e9ec45a
Tommaso <soncintommaso@gmail.com>
2024-03-07 11:48:46 +0100

    POST register route

--- taw-backend/src/routes/auth.ts
+++ taw-backend/src/routes/auth.ts
@@ -1,8 +1,8 @@
-import { Router } from 'express'
-import { login } from '../controllers/auth'
-import { register } from "../controllers/register"
-const authRoutes:Router = Router()
+import { Router } from "express";
+import { login } from "../controllers/auth";
+import { register } from "../controllers/register";
+const authRoutes: Router = Router();
 
-authRoutes.get( '/login', login )
-authRoutes.get("/register", register)
-export default authRoutes
+authRoutes.get("/login", login);
+authRoutes.post("/register", register);
+export default authRoutes;
