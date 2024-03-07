0cc3fce62f71db03d8f9f00f0825ae61a8b16289
Tommaso <soncintommaso@gmail.com>
2024-03-07 15:06:08 +0100

    feat: add email check for already registered user, add password === check

--- taw-backend/src/controllers/register.ts
+++ taw-backend/src/controllers/register.ts
@@ -24,26 +24,38 @@ const validateForm = (input: unknown) => {
     const isValid = formValidator.parse(input);
     return isValid;
   } catch (e) {
-    if (e instanceof z.ZodError) {
-      console.log("Error while validating input:", e.issues);
-    }
     return false;
   }
 };
 export const registerController = async (req: Request, res: Response) => {
-  const isValid = validateForm(req.body);
+  const isValid =
+    validateForm(req.body) && req.body.password === req.body.confirmPassword;
   if (!isValid) {
-    return res.status(404).send("User input error.");
+    return res.status(400).send("ERROR: Invalid form input.");
   }
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(req.body.password, salt);
-  await prisma.user.create({
-    data: {
-      name: req.body.name ?? "",
+  const alreadyRegisteredUser = await prisma.user.findFirst({
+    where: {
       email: req.body.email,
-      password: hashedPassword,
-      role: req.body.role,
     },
   });
-  return res.status(200).send("OK");
+  if (alreadyRegisteredUser !== null) {
+    return res.status(400).send("ERROR: User already registered.");
+  }
+  try {
+    await prisma.user.create({
+      data: {
+        name: req.body.name ?? "",
+        email: req.body.email,
+        password: hashedPassword,
+        role: req.body.role,
+      },
+    });
+    return res.status(200).send("OK");
+  } catch (e) {
+    return res
+      .status(503)
+      .send("ERROR: Can't create new user. Please try later.");
+  }
 };
