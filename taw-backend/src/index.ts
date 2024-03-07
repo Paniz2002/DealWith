11af2368fa4b358ffa2789132af609fa14be55f1
Tommaso <soncintommaso@gmail.com>
2024-03-07 12:26:13 +0100

    removed / route

--- taw-backend/src/index.ts
+++ taw-backend/src/index.ts
@@ -8,22 +8,22 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
-
 const whitelist = ["localhost", "127.0.0.1", "::1", "::ffff:127.0.0.1"];
 const corsOptions = {
   origin: "localhost",
 };
 
-function whitelistMiddleware(req: Request, res: Response, next: NextFunction) {
+const whitelistMiddleware = function (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
   const remoteAddr = req.socket.remoteAddress;
   console.log(remoteAddr);
   if (whitelist.includes(remoteAddr!)) {
     next();
   }
-}
+};
 app.use(cors(corsOptions));
 app.use("/api", whitelistMiddleware, rootRouter);
 
