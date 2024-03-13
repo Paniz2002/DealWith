a5adb22c570f0979e4620888a7294c1c0c74e53a
Tommaso <soncintommaso@gmail.com>
2024-03-13 15:47:15 +0100

    current dump, working on frontend validation

--- taw-frontend/src/app/register/register.component.ts
+++ taw-frontend/src/app/register/register.component.ts
@@ -1,37 +1,50 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
   FormsModule,
   ReactiveFormsModule,
+  Validators,
 } from '@angular/forms';
-import { enviroment } from '../../enviroments/enviroments';
+import { enviroments } from '../../enviroments/enviroments';
 import axios from 'axios';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {
-  registerForm: FormGroup;
-  constructor(private registerFormBuilder: FormBuilder) {
+export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
+  isFormValid!: Boolean;
+  constructor(private registerFormBuilder: FormBuilder) {}
+  ngOnInit() {
+    this.isFormValid = false;
     this.registerForm = this.registerFormBuilder.group({
-      name: [''],
-      email: [''],
-      password: [''],
-      confirmPassword: [''],
+      username: ['', Validators.required, Validators.minLength(1)],
+      password: ['', Validators.required, Validators.minLength(8)],
+      confirmPassword: ['', Validators.required, Validators.minLength(8)],
       role: [''],
     });
   }
+
   async onSubmit() {
-    const url = enviroment.BACKEND_URL + '/api/auth/register';
+    const url = enviroments.BACKEND_URL + '/api/auth/register';
     const res = await axios.post(url, this.registerForm.value);
     if (res.status !== 200) {
       console.log('Error: ', res.statusText);
     }
   }
-
 }
