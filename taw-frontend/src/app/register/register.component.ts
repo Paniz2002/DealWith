70a363d219dda03ff246c041b66ce0c0440e58a9
Tommaso <soncintommaso@gmail.com>
2024-03-14 15:15:52 +0100

    injected breakpointobserver

--- taw-frontend/src/app/register/register.component.ts
+++ taw-frontend/src/app/register/register.component.ts
@@ -14,6 +14,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 @Component({
   selector: 'app-register',
   standalone: true,
@@ -33,7 +34,10 @@ import { MatIconModule } from '@angular/material/icon';
 export class RegisterComponent implements OnInit {
   registerForm!: FormGroup;
   isFormValid!: Boolean;
-  constructor(private registerFormBuilder: FormBuilder) {}
+  constructor(
+    private registerFormBuilder: FormBuilder,
+    private responsiveHandler: BreakpointObserver,
+  ) {}
   ngOnInit() {
     this.isFormValid = false;
     this.registerForm = this.registerFormBuilder.group({
