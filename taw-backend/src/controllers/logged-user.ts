cd21e9c1dad8228e02b7698c37697731d2d18c86
Tommaso <soncintommaso@gmail.com>
2024-03-30 15:24:49 +0100

    refactored GET /me route

--- taw-backend/src/controllers/logged-user.ts
+++ taw-backend/src/controllers/logged-user.ts
@@ -1,6 +1,20 @@
 import { Response } from "express";
+import prisma from "../../prisma/prisma_db_connection";
+import { JWT_SECRET } from "../secret";
+import { verify } from "jsonwebtoken";
 
 export const loggedUserController = async (req: any, res: Response) => {
-  res.json(req.user);
+  const { id, role } = verify(req.headers.authorization, JWT_SECRET) as any;
+  const user = await prisma.user.findFirst({
+    where: {
+      id: id,
+      role: role,
+    },
+    select: {
+      id: true,
+      role: true,
+      username: true,
+    },
+  });
+  return res.json(user);
 };
-
