1e33e5c9a832d4f8c29b3a3286fd153b8bd65075
Tommaso <soncintommaso@gmail.com>
2024-04-02 11:30:51 +0200

    skeleton of cashier-homepage angular component

--- taw-frontend/src/app/components/cashier-homepage/cashier-homepage.component.ts
+++ taw-frontend/src/app/components/cashier-homepage/cashier-homepage.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
+import { MatTabsModule } from '@angular/material/tabs';
 @Component({
   selector: 'app-cashier-homepage',
   standalone: true,
-  imports: [],
+  imports: [MatTabsModule],
   templateUrl: './cashier-homepage.component.html',
   styleUrl: './cashier-homepage.component.css',
 })
-export class CashierHomepageComponent {}
+export class CashierHomepageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
