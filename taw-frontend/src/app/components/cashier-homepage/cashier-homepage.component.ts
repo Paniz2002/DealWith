f30fac0e79326440dcc0f7fe400cf9652bd9e0db
Tommaso <soncintommaso@gmail.com>
2024-05-18 16:04:06 +0200

    commit before 2024 project

--- taw-frontend/src/app/components/cashier-homepage/cashier-homepage.component.ts
+++ taw-frontend/src/app/components/cashier-homepage/cashier-homepage.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import axios from 'axios';
 import { MatTabsModule } from '@angular/material/tabs';
+import { enviroments } from '../../../enviroments/enviroments';
+import { LocalStorageService } from '../../services/localStorage/localStorage.service';
+import { NotificationService } from '../../services/popup/notification.service';
+import { TableStatusCardComponent } from '../table-status-card/table-status-card.component';
 @Component({
   selector: 'app-cashier-homepage',
   standalone: true,
-  imports: [MatTabsModule],
+  imports: [MatTabsModule, TableStatusCardComponent],
   templateUrl: './cashier-homepage.component.html',
   styleUrl: './cashier-homepage.component.css',
 })
 export class CashierHomepageComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private localStorage: LocalStorageService,
+    private snackBar: NotificationService,
+  ) {}
+  freeTables = [];
+  occupiedTables = [];
+  ngOnInit(): void {
+    axios
+      .get(enviroments.BACKEND_URL + '/api/common/tables', {
+        params: {
+          status: 'free',
+        },
+        headers: {
+          Authorization: this.localStorage.get('jwt'),
+        },
+      })
+      .then((res) => {
+        if (res.status === 200) this.freeTables = res.data;
+      })
+      .catch((err) => {
+        if (axios.isAxiosError(err)) this.snackBar.notify(err.message);
+      });
 
-  ngOnInit(): void {}
+    axios
+      .get(enviroments.BACKEND_URL + '/api/common/tables', {
+        params: {
+          status: 'occupied',
+        },
+        headers: {
+          Authorization: this.localStorage.get('jwt'),
+        },
+      })
+      .then((res) => {
+        if (res.status === 200) this.occupiedTables = res.data;
+      })
+      .catch((err) => {
+        if (axios.isAxiosError(err)) this.snackBar.notify(err.message);
+      });
+  }
 }
--- taw-frontend/src/enviroments/enviroments.ts
+++ taw-frontend/src/enviroments/enviroments.ts
@@ -1,5 +1,3 @@
 export const enviroments = {
   BACKEND_URL: 'http://localhost:3000',
-  JWT_SECRET:
-    '472260c93c70c5be997c620cd5d30fd4000b368e3abf9b6962c39e1ca74214db',
 };
