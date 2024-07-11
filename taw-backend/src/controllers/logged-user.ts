abf80074d590814abe09bdf0b53c68f44d652258
Tommaso <soncintommaso@gmail.com>
2024-07-11 15:00:18 +0200

    trying to adhere to owasp standards

--- taw-backend/src/controllers/logged-user.ts
+++ taw-backend/src/controllers/logged-user.ts
@@ -1,43 +1,42 @@
-import {Response} from "express";
-import {JWT_SECRET} from "../secret";
-import {verify} from "jsonwebtoken";
+import { Response } from "express";
+import { JWT_SECRET } from "../secret";
 import UserModel from "../../models/user";
 import connectDB from "../../config/db";
 import BadRequestException from "../exceptions/bad-request";
 import * as jwt from "jsonwebtoken";
 import UnautorizedException from "../exceptions/unauthorized";
 
-export const loggedUserController = async (req: any, res: Response) => {
-    connectDB();
-    //  1.   extract the token from header
-    const token = req.cookies.jwt;
+export const checkJWTController = async (req: any, res: Response) => {
+  connectDB();
+  //  1.   extract the token from header
+  const token = req.cookies.jwt;
 
-    //  2.   if token is not present, throw a bad request exception.
-    if (!token) {
-        // Next prints to console ... because reasons.
-        // So i modified it accordingly
-        return BadRequestException(req, res, "Bad Request: Missing JWT");
+  //  2.   if token is not present, throw a bad request exception.
+  if (!token) {
+    // Next prints to console ... because reasons.
+    // So i modified it accordingly
+    return BadRequestException(req, res, "Bad Request: Missing JWT");
+  }
+
+  try {
+    //  3.   if token is present, decode the token and extract the payload
+    const payload = jwt.verify(token, JWT_SECRET) as any;
+    const user = await UserModel.findOne({
+      _id: payload._id,
+    }).populate("email_id");
+    if (!user) {
+      return UnautorizedException(req, res, "Unauthorized: Invalid JWT");
     }
-
-    try {
-        //  3.   if token is present, decode the token and extract the payload
-        const payload = jwt.verify(token, JWT_SECRET) as any;
-        const user = await UserModel.findOne({
-            '_id': payload._id
-        }).populate('email_id');
-        if (!user) {
-            return UnautorizedException(req, res, "Unauthorized: Invalid JWT");
-        }
-        let response_json = {
-            username: user.username,
-            is_moderator: user.isModerator(),
-            needs_update: user.isModerator() && (user.createdAt.getTime() === user.updatedAt.getTime()),
-            email_address: user.email_id.address
-        };
-        return res.json(response_json);
-    } catch (error) {
-        console.log(error)
-        return UnautorizedException(req, res, "Unauthorized: Unknown error.");
-    }
-
+    let response_json = {
+      username: user.username,
+      is_moderator: user.isModerator(),
+      needs_update:
+        user.isModerator() &&
+        user.createdAt.getTime() === user.updatedAt.getTime(),
+      email_address: user.email_id.address,
+    };
+    return res.json(response_json);
+  } catch (error) {
+    return UnautorizedException(req, res, "Unauthorized: Unknown error.");
+  }
 };
--- taw-backend/src/controllers/login.ts
+++ taw-backend/src/controllers/login.ts
@@ -14,28 +14,22 @@ export const loginController = async (req: Request, res: Response) => {
   let user = await UserModel.findOne({
     username: username,
   });
-  if (!user) {
-    return NotFoundException(req, res, "User does not exists");
+  if (!user || !user.comparePassword(password)) {
+    return BadRequestException(req, res, "Invalid credentials.");
   }
 
-  if (!user.comparePassword(password)) {
-    return BadRequestException(req, res, "Invalid Password");
-  }
-
-  let response_json = {
+  let response = {
     _id: user.id,
     username: user.username as string,
     is_moderator: user.isModerator(),
-    needs_update:
-      user.isModerator() &&
-      user.createdAt.getTime() === user.updatedAt.getTime(),
+    needs_update: user.createdAt.getTime() === user.updatedAt.getTime(),
   };
 
-  const token = jwt.sign(response_json, JWT_SECRET);
-  res.cookie("jwt", token, {
+  const jwtToken = jwt.sign(response, JWT_SECRET);
+  res.cookie("jwt", jwtToken, {
     httpOnly: true,
     secure: false,
     sameSite: "lax",
   });
-  return res.json(response_json);
+  return res.json(response);
 };
