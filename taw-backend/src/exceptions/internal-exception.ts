0c2ced833531686fba0183cc1296821ad6236aba
Tommaso <soncintommaso@gmail.com>
2024-07-11 14:27:34 +0200

    fix: InternalException status code

--- taw-backend/src/exceptions/internal-exception.ts
+++ taw-backend/src/exceptions/internal-exception.ts
@@ -1,9 +1,8 @@
 import { Request, Response } from "express";
 const InternalException = (req: Request, res: Response, message: string) => {
-  return res.status(401).json({
+  return res.status(500).json({
     message: message,
   });
 };
 
 export default InternalException;
-
