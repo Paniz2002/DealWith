c6a02e3ff0128041b633286a91e69b355139f3ff
Tommaso <soncintommaso@gmail.com>
2024-07-15 13:48:52 +0200

    fix: manual password hashing

--- taw-backend/src/controllers/profile.ts
+++ taw-backend/src/controllers/profile.ts
@@ -4,38 +4,39 @@ import { JWT_SECRET } from "../secret";
 import UserModel from "../../models/user";
 import InternalException from "../exceptions/internal-exception";
 import BadRequestException from "../exceptions/bad-request";
-import { compareSync } from "bcryptjs";
-export const updateProfileController = (req: Request, res: Response) => {
+import { compareSync, hashSync } from "bcryptjs";
+
+interface UpdateProfile {
+  password?: string;
+  updatedAt: Date;
+}
+export const updateProfileController = async (req: Request, res: Response) => {
   const { oldPassword, password, confirmPassword } = req.body;
   if (oldPassword === password || password !== confirmPassword) {
     return BadRequestException(req, res, "Invalid credentials.");
   }
-  // I don't like this.
-  // TODO: create interface when we're all done with this project.
-  const update = {} as any;
+  const update: UpdateProfile = {
+    updatedAt: new Date(),
+  };
   const jwtToken = req.cookies.jwt;
   const jwtBody = jwt.verify(jwtToken, JWT_SECRET) as any;
   const userID = jwtBody._id;
   if (password !== undefined) {
-    update.password = password;
+    // We have to do this by hand because mongoose does not support
+    // .pre on update, only on creation and when we do .save()
+    update.password = hashSync(password, 10);
   }
   try {
-    UserModel.findById(userID)
-      .exec()
-      .then((user) => {
-        if (compareSync(oldPassword, user.password)) {
-          UserModel.updateOne(
-            {
-              _id: userID,
-            },
-            update,
-            {
-              runValidators: true,
-            },
-          );
-          return res.sendStatus(200);
-        } else return BadRequestException(req, res, "Invalid credentials.");
-      });
+    const user = await UserModel.findById(userID).exec();
+    if (!user || !compareSync(oldPassword, user.password)) {
+      return BadRequestException(req, res, "Invalid credentials.");
+    }
+
+    await UserModel.updateOne({ _id: userID }, update, {
+      runValidators: true,
+    });
+
+    return res.sendStatus(200);
   } catch (e) {
     return InternalException(req, res, "Unknown error while updating profile.");
   }
