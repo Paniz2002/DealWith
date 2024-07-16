3c380006c66a590c619cb1aa28145209a9ee0a39
Tommaso <soncintommaso@gmail.com>
2024-07-16 12:54:06 +0200

    feat: backend GET /api/common/students
    This API retrieves all the students.

--- taw-backend/src/routes/index.ts
+++ taw-backend/src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import authRoutes from "./auth";
+import commonRoutes from "./common";
 const apiRouter: Router = Router();
 
 apiRouter.use("/auth", authRoutes);
+apiRouter.use("/common", commonRoutes);
 export default apiRouter;
