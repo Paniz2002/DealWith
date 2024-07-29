f0ad0ca09deba9707d277a685213e51a3bbe252b
Tommaso <soncintommaso@gmail.com>
2024-07-29 12:13:14 +0200

    refactor: moving container on app page

--- taw-frontend/src/app/components/header/header.component.ts
+++ taw-frontend/src/app/components/header/header.component.ts
@@ -1,21 +1,14 @@
 import { Component } from '@angular/core';
-import {MatToolbar} from "@angular/material/toolbar";
-import {MatIcon} from "@angular/material/icon";
-import {MatButton} from "@angular/material/button";
-import {RouterLink} from "@angular/router";
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [
-    MatToolbar,
-    MatIcon,
-    MatButton,
-    RouterLink
-  ],
+  imports: [MatToolbar, MatIcon, MatButton, RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
