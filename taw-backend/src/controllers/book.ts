43d3cbd0d005a8034d32025969b8471c372048b0
Paniz2002 <892678@stud.unive.it>
2024-07-29 19:31:36 +0200

    fixed book controller structure

--- taw-backend/src/controllers/book.ts
+++ taw-backend/src/controllers/book.ts
@@ -3,8 +3,11 @@ import connectDB from "../../config/db";
 
 import InternalException from "../exceptions/internal-exception";
 import Book from "../../models/book";
+import NotFoundException from "../exceptions/not-found";
 
-async function searchBooks(to_search: string | undefined) {
+
+/*
+async function searchBooks(req:Request, res:Response,to_search: string | undefined) {
     await connectDB();
     try {
         let books = null;
@@ -12,6 +15,9 @@ async function searchBooks(to_search: string | undefined) {
             books = await Book.find({
                 $text: {$search: to_search}
             });
+
+            if(!books)
+                return NotFoundException(req, res, "No books found.");
         } else {
             books = await Book.find();
         }
@@ -27,21 +33,47 @@ async function searchBooks(to_search: string | undefined) {
             });
         }
     } catch (err) {
-        console.error('Error performing search:', err);
-        throw err;
+        return InternalException(req, res, "Unknown error while searching books.");
     }
 
+}
+*/
+
+
+export const searchBooks = async (req: Request, res: Response) => {
+    const text = req.params['text'];
+    // return res.status(200).json(text);
+    await connectDB();
+
+    try {
+        let books = await Book.find({
+            $text: {$search: text}
+        });
+
+        if(!books)
+            return NotFoundException(req, res, "No books found.");
+
+
+        //take only id, name year and ISBN
+        return books.map((book) => {
+            return {
+                id: book._id,
+                title: book.title,
+                year: book.year,
+                ISBN: book.ISBN
+            }
+        });
+
+    } catch (err) {
+        return InternalException(req, res, "Unknown error while searching books.");
+    }
 }
 
 export const getBooks = async (req: Request, res: Response) => {
     try {
-        let to_search = req.query.q;
-        if(to_search){
-            to_search = to_search.toString();
-        }
-        const books = await searchBooks(to_search);
+        const books = await Book.find();
         return res.status(200).json(books);
     } catch (err) {
-        return InternalException(req, res, "Unknown error while creating listing");
+        return InternalException(req, res, "Unknown error while getting books.");
     }
 };
--- taw-backend/src/routes/book.ts
+++ taw-backend/src/routes/book.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { studentMiddleware } from "../middlewares/student";
-import { getBooks } from "../controllers/book";
+import { getBooks, searchBooks } from "../controllers/book";
 
 // students only
 const bookRoutes: Router = Router();
 bookRoutes.get("/", [studentMiddleware] , getBooks); // get all books or filter by string q
+bookRoutes.get("/:text", [studentMiddleware] , searchBooks); // get book by id
 export default bookRoutes;
