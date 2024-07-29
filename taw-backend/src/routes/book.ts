a0139b7ef6023dd943b243443125bdbd8965c1fd
Tommaso <soncintommaso@gmail.com>
2024-07-29 23:50:27 +0200

    chore: formatter

--- taw-backend/src/routes/book.ts
+++ taw-backend/src/routes/book.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import { studentMiddleware } from "../middlewares/student";
-import { getBooks } from "../controllers/book";
+import { getBooksController } from "../controllers/book";
 
 // students only
 const bookRoutes: Router = Router();
-bookRoutes.get("/", [studentMiddleware] , getBooks); // get all books or filter by string q
+bookRoutes.get("/", [studentMiddleware], getBooksController); // get all books or filter by string q
 export default bookRoutes;
