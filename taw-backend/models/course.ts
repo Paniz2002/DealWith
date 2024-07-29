bec05e637cb3485f63b63e73967efe41d2ce1923
Tommaso <soncintommaso@gmail.com>
2024-07-29 23:50:51 +0200

    feat: created index for course and university model name

--- taw-backend/models/course.ts
+++ taw-backend/models/course.ts
@@ -1,25 +1,27 @@
-import mongoose, { Model } from 'mongoose';
+import mongoose, { Model } from "mongoose";
 
 const CourseSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "can't be blank"],
+  name: {
+    type: String,
+    required: [true, "can't be blank"],
+    index: true,
+  },
+  year: {
+    type: { year1: Number, year2: Number },
+    required: [true, "can't be blank"],
+    index: true,
+  },
+  university: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "University",
+  },
+  auctions: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Auction",
     },
-    year: {
-        type: { year1: Number, year2: Number},
-        required: [true, "can't be blank"],
-        index: true
-    },
-    university: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'University'
-    },
-    auctions: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Auction'
-    }]
+  ],
 });
-
-const Course: Model<any> = mongoose.model('Course', CourseSchema);
-
+const Course: Model<any> = mongoose.model("Course", CourseSchema);
 export default Course;
+
--- taw-backend/models/university.ts
+++ taw-backend/models/university.ts
@@ -1,25 +1,28 @@
-import mongoose, { Model } from 'mongoose';
-import uniqueValidator from 'mongoose-unique-validator';
+import mongoose, { Model } from "mongoose";
+import uniqueValidator from "mongoose-unique-validator";
 
 const UniversitySchema = new mongoose.Schema({
-    name: {
-        type: String,
-        unique: true,
-        required: [true, "can't be blank"],
-        index:true
+  name: {
+    type: String,
+    unique: true,
+    required: [true, "can't be blank"],
+    index: true,
+  },
+  city: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "City",
+  },
+  courses: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
     },
-    city:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'City'
-    },
-    courses: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Course'
-    }]
+  ],
 });
 
-UniversitySchema.plugin(uniqueValidator, { message: 'already exists' });
+UniversitySchema.plugin(uniqueValidator, { message: "already exists" });
 
-const University: Model<any> = mongoose.model('University', UniversitySchema);
+const University: Model<any> = mongoose.model("University", UniversitySchema);
 
 export default University;
+
