6f2dc6e20b858646a94b65a7e39d36ec74f053bd
Paniz2002 <892678@stud.unive.it>
2024-08-01 20:27:29 +0200

    finished GET /auctions with params and merged in one query

--- taw-backend/src/controllers/auction.ts
+++ taw-backend/src/controllers/auction.ts
@@ -8,10 +8,6 @@ import Auction from "../../models/auction";
 import Book from "../../models/book";
 import User from "../../models/user";
 import {fullTextSearch, getUserId, validateForm} from "../index";
-import {HydratedDocument, Query, QueryWithHelpers} from 'mongoose';
-
-// TODO caricare le immagini e gestirle
-// https://chatgpt.com/share/c6a5221d-83d5-444b-8939-920f39c2d22e
 
 const formValidator = z.object({
     condition: z.enum(['Mint', 'Near Mint', 'Excellent', 'Good', 'Fair', 'Poor']),
@@ -23,14 +19,14 @@ const formValidator = z.object({
     description: z.string().optional(),
     book_id: z.string()
 }).refine(data => data.start_date < data.end_date, {
-    message: "End date must be greater than start date", path: ['start_date','end_date']
+    message: "End date must be greater than start date", path: ['start_date', 'end_date']
 });
 
 export const newAuctionController = async (req: Request, res: Response) => {
-    try{
+    try {
         req.body.start_date = new Date(req.body.start_date);
         req.body.end_date = new Date(req.body.end_date);
-    }catch (e){
+    } catch (e) {
         return BadRequestException(req, res, "Invalid date format");
     }
     validateForm(req, res, req.body, formValidator);
@@ -75,9 +71,8 @@ export const newAuctionController = async (req: Request, res: Response) => {
 }
 
 
-
 export const uploadAuctionImagesController = async (req: Request, res: Response) => {
-    if(!req.files){
+    if (!req.files) {
         return BadRequestException(req, res, "No images uploaded");
     }
 
@@ -85,29 +80,29 @@ export const uploadAuctionImagesController = async (req: Request, res: Response)
 
     const {auction_id, seller_id} = req.body;
 
-    if(!auction_id || !seller_id){
+    if (!auction_id || !seller_id) {
         return BadRequestException(req, res, "Missing auction_id or user_id");
     }
 
     const auction = await Auction.findById(auction_id);
     const user = await User.findById(seller_id);
-    if(!auction || !user){
+    if (!auction || !user) {
         return BadRequestException(req, res, "Auction or user not found");
     }
 
     const user_id = getUserId(req, res);
 
-    if(auction.isOwner(user_id)){
+    if (auction.isOwner(user_id)) {
         return BadRequestException(req, res, "The seller is not the owner of the auction");
     }
 
-    if(auction.seller.toString() !== user_id){
+    if (auction.seller.toString() !== user_id) {
         return UnauthorizedException(req, res, "Unauthorized: You are not the seller of this auction");
     }
 
     const files = req.files as Express.Multer.File[];
-    try{
-        for(const file of files){
+    try {
+        for (const file of files) {
             const path = file.path;
             auction.images.push(path);
         }
@@ -115,7 +110,7 @@ export const uploadAuctionImagesController = async (req: Request, res: Response)
         await auction.save();
 
         return res.status(200).send("Images uploaded");
-    }catch (e){
+    } catch (e) {
         return InternalException(req, res, "Error while saving images");
     }
 }
@@ -127,7 +122,7 @@ const queryValidator = z.object({
     condition: z.enum(['Mint', 'Near Mint', 'Excellent', 'Good', 'Fair', 'Poor']).optional(),
 });
 
-const getSuperiorConditions = function(currentCondition: string) {
+const getSuperiorConditions = function (currentCondition: string) {
     const conditions = ['Mint', 'Near Mint', 'Excellent', 'Good', 'Fair', 'Poor'];
 
     const index = conditions.indexOf(currentCondition);
@@ -137,130 +132,107 @@ const getSuperiorConditions = function(currentCondition: string) {
     return conditions.slice(0, index + 1);
 }
 
+const searchAuctions = async function (req: Request, res: Response) {
+    try {
 
-/* TODO populate auction in different function */
-const searchAuctions = async function(q: any, req: Request, res: Response) {
-    try{
-        const allAuctions = await Auction.find();
-        const books = await fullTextSearch(Book, q);
-        const searchedAuctions = await fullTextSearch(Auction, q);
-        for(let auction of allAuctions){
-            const exists = books.find(book => book._id.toString() === auction.book.toString());
-            // const exists = books.includes(auction.book.toString());
-            console.log(exists);
-            if (exists && !searchedAuctions.includes(auction)){
-                searchedAuctions.push(auction);
+        const {
+            q,
+            min_starting_price,
+            max_starting_price,
+            min_reserve_price,
+            max_reserve_price,
+            min_condition,
+            active
+        } = req.query;
+
+        let superior_conditions = ['Mint', 'Near Mint', 'Excellent', 'Good', 'Fair', 'Poor'];
+        if (min_condition) {
+            try {
+                superior_conditions = getSuperiorConditions(min_condition.toString());
+            } catch (e) {
+                console.log(e);
+                return BadRequestException(req, res, "Invalid condition");
             }
         }
-        if(!searchedAuctions){
-            return BadRequestException(req, res, "No auctions found");
-        }
+        let searchedAuctions = [];
+        const allAuctions = await Auction.find();
+        if (q) {
+            const books = await fullTextSearch(Book, q.toString());
+            searchedAuctions = await fullTextSearch(Auction, q.toString());
+            for (let auction of allAuctions) {
+                const exists = books.find(book => book._id.toString() === auction.book.toString());
 
+                if (exists && !searchedAuctions.includes(auction)) {
+                    searchedAuctions.push(auction);
+                }
+            }
+            if (!searchedAuctions) {
+                return BadRequestException(req, res, "No auctions found");
+            }
+        }
         let auctions = await Auction.find(
             {
-                _id: {$in: searchedAuctions.map(auction => auction._id)}
+                $and: [
+                    {
+                        starting_price: min_starting_price ? {$gte: min_starting_price} : max_starting_price ? {$lte: max_starting_price} : min_starting_price && max_starting_price ? {
+                            $gte: min_starting_price,
+                            $lte: max_starting_price
+                        } : {$gte: 0}
+                    },
+                    {
+                        reserve_price: min_reserve_price ? {$gte: min_reserve_price} : max_reserve_price ? {$lte: max_reserve_price} : min_reserve_price && max_reserve_price ? {
+                            $gte: min_reserve_price,
+                            $lte: max_reserve_price
+                        } : {$gte: 0}
+                    },
+                    {condition: {$in: superior_conditions}},
+                    {end_date: active ? {$gt: Date.now()} : {$gt: new Date('1970-01-01T00:00:00Z')} },
+                    {_id: q ? {$in: searchedAuctions.map(auction => auction._id)} : {$in: allAuctions.map(auction => auction._id)}},
+                ]
             }).populate({
             path: 'book',
-            populate: {path: 'courses', select: '-_id -__v -auctions -books -year._id',
-                populate:{path: 'university', select: '-_id -__v -courses ',
-                    populate: {path: 'city', select: '-_id -__v -universities -courses'}}},
+            populate: {
+                path: 'courses', select: '-_id -__v -auctions -books -year._id',
+                populate: {
+                    path: 'university', select: '-_id -__v -courses ',
+                    populate: {path: 'city', select: '-_id -__v -universities -courses'}
+                }
+            },
             select: '-_id -__v -auctions'
         }).populate({
             path: 'seller',
-            select:'-__v -_id -password -email -role'
+            select: '-__v -_id -password -email -role'
         }).select('-_id -__v');
 
-        if(!auctions){
+        if (!auctions) {
             return BadRequestException(req, res, "No auctions found");
-        }else{
+        } else {
             return auctions;
         }
 
-
-
-    }catch(err){
+    } catch (err) {
         return InternalException(req, res, "Error while searching auctions");
     }
 }
 
-const searchAuctionsWithParams= async function(req: Request, res: Response){
-    const {
-        min_starting_price,
-        max_starting_price,
-        min_reserve_price,
-        max_reserve_price,
-        min_condition,
-        active
-    } = req.query;
-    // se le condizioni minime non sono specificate le ritorno tutte
-    let superior_conditions = ['Mint', 'Near Mint', 'Excellent', 'Good', 'Fair', 'Poor'];
-    if(min_condition){
-        try{
-            superior_conditions = getSuperiorConditions(min_condition.toString());
-        }catch(e){
-            console.log(e);
-            return BadRequestException(req, res, "Invalid condition");
-        }
-    }
-
-    const auctions = await Auction.find({
-        $and: [
-            {
-                starting_price: min_starting_price ? {$gte: min_starting_price} : max_starting_price ? {$lte: max_starting_price} : min_starting_price && max_starting_price ? {
-                    $gte: min_starting_price,
-                    $lte: max_starting_price
-                } : {$gte: 0}
-            },
-            {
-                reserve_price: min_reserve_price ? {$gte: min_reserve_price} : max_reserve_price ? {$lte: max_reserve_price} : min_reserve_price && max_reserve_price ? {
-                    $gte: min_reserve_price,
-                    $lte: max_reserve_price
-                } : {$gte: 0}
-            },
-            {condition: {$in: superior_conditions}},
-            {end_date: active ? {$gt: Date.now()} : {$lt: Date.now()} || {$gt: Date.now()}},
-        ]
-    }).populate({
-        path: 'book',
-        populate: {path: 'courses', select: '-_id -__v -auctions -books -year._id',
-            populate:{path: 'university', select: '-_id -__v -courses ',
-                populate: {path: 'city', select: '-_id -__v -universities -courses'}}},
-        select: '-_id -__v -auctions'
-    }).populate({
-        path: 'seller',
-        select:'-__v -_id -password -email -role'
-    }).select('-_id -__v');
-
-    if(!auctions){
-        return BadRequestException(req, res, "No auctions found");
-    }else{
-        return auctions;
-    }
-
-}
-
 export const getAuctionController = async (req: Request, res: Response) => {
-    if(Object.keys(req.query).length === 0){
+    if (Object.keys(req.query).length === 0) {
         const auctions = await Auction.find().select('-_id');
         return res.status(200).json(auctions);
     }
 
     validateForm(req, res, req.query, queryValidator);
 
-    try{
-        let auctions = await searchAuctionsWithParams(req,res);
+    try {
+        let auctions = await searchAuctions(req, res);
 
-        if(req.query.q){
-            auctions = await searchAuctions(req.query.q, req, res);
-        }
-
-        if(!auctions) {
+        if (!auctions) {
             return BadRequestException(req, res, "No auctions found");
         }
 
         return res.status(200).json(auctions);
 
-    }catch (e){
+    } catch (e) {
         console.log(e);
         return InternalException(req, res, "Error while returning auctions");
     }
