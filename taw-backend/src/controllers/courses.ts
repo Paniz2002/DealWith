fae89227543fb47ce853a7c6859bfbe6de9d9194
Tommaso <soncintommaso@gmail.com>
2024-08-02 12:07:13 +0200

    working on GET courses

--- taw-backend/src/controllers/courses.ts
+++ taw-backend/src/controllers/courses.ts
@@ -1,73 +1,46 @@
 import { Request, Response } from "express";
-import connectDB from "../../config/db";
+import connectDB from "../config/db";
 import University from "../../models/university";
-import { AggregateOptions, PipelineStage } from "mongoose";
-import BadRequestException from "../exceptions/bad-request";
 import Course from "../../models/course";
-import InternalException from "../exceptions/internal-exception";
-
+import City from "../../models/city";
+import BadRequestException from "../exceptions/bad-request";
+import fullTextSearch from "../utils/search";
+interface UniversityResult {
+  _id: string | null;
+  name: string;
+  city: string;
+  courses: string[];
+  __v: number;
+}
 /*
  * GET /api/courses
  * @params university: String University to be searched.
  * @params course: String Course to be searched.
  */
 export const getCoursesController = async (req: Request, res: Response) => {
-  // Imagine doing { cityID : String } = req.body
-  // Instead of doing this:
-  const { university, course } = req.query;
+  let { university, course } = req.query;
   if (university == undefined && course == undefined) {
-    return BadRequestException(req, res, "Bad request: invalid parameters.");
+    return BadRequestException(req, res, "Bad request: Invalid parameters.");
   }
+  university = university as string;
+  course = course as string;
   await connectDB();
   if (university) {
-    try {
-      const pipeline: PipelineStage[] = [
-        { $match: { $text: { $search: university } } },
-        {
-          $lookup: {
-            from: "courses",
-            localField: "_id",
-            foreignField: "university",
-            as: "courses",
-          },
-        },
-      ];
-
-      const results = await University.aggregate(pipeline).exec();
-      return res.status(200).json(results);
-    } catch (error) {
-      return InternalException(req, res, "Unknown error while retrieving data");
-    }
+    const result = await fullTextSearch(University, university)
+      .populate({
+        path: "city",
+        model: City,
+        select: "name province country -_id",
+      })
+      .populate({
+        path: "courses",
+        model: Course,
+        select: "name year1 year2 -_id",
+      })
+      .exec();
+    return res.status(200).json(result);
   }
-
   if (course) {
-    try {
-      const pipeline = [
-        { $match: { $text: { $search: course } } },
-        {
-          $lookup: {
-            from: "universities",
-            localField: "university",
-            foreignField: "_id",
-            as: "university",
-          },
-        },
-        { $unwind: "$university" },
-        {
-          $lookup: {
-            from: "courses",
-            localField: "university._id",
-            foreignField: "university",
-            as: "allCourses",
-          },
-        },
-      ];
-
-      const results = await Course.aggregate(pipeline).exec();
-      return res.status(200).json(results);
-    } catch (error) {
-      return InternalException(req, res, "Unknown error while retrieving data");
-    }
   }
 };
 export const postCoursesController = (req: Request, res: Response) => {};
