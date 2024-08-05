bdbbe55e2eb89ac021ae1c143152f5443c51fa86
Tommaso Soncin <soncintommaso@gmail.com>
2024-08-05 15:46:42 +0200

    feat: auction card component

--- taw-frontend/src/app/components/auction-card/auction-card.component.ts
+++ taw-frontend/src/app/components/auction-card/auction-card.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-auction-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, MatCardModule, MatButtonModule],
   templateUrl: './auction-card.component.html',
-  styleUrl: './auction-card.component.css'
+  styleUrl: './auction-card.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AuctionCardComponent {
-
+  @Input({ required: true }) auctionID!: string;
+  @Input({ required: true }) bookTitle!: string;
+  @Input({ required: true }) bookAuthor!: string;
+  @Input({ required: true }) bookDescription!: string;
+  @Input() base64Image?: string;
 }
