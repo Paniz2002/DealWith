a6208cb178cc220cbdcf9694e54d1eb9fda1f19f
michael <michael@busnet.it>
2024-08-21 01:11:29 +0200

    See notification + delete notification + click notification to go to auction + tring to fix calendar on create auction (not done)

--- taw-frontend/src/app/components/header/header.component.ts
+++ taw-frontend/src/app/components/header/header.component.ts
@@ -1,22 +1,29 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Component, OnInit, OnDestroy, ViewChildren, QueryList, ElementRef} from '@angular/core';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatIcon} from '@angular/material/icon';
-import {MatButton} from '@angular/material/button';
-import {RouterLink} from '@angular/router';
+import {MatButton, MatIconButton} from '@angular/material/button';
+import {Router, RouterLink} from '@angular/router';
 import {LocalStorageService} from '../../services/localStorage/localStorage.service';
 import {Subscription} from 'rxjs';
 import {EventManagerService} from '../../services/eventManager/event-manager.service';
 import {SocketService} from "../../socket.service";
 import axios from "axios";
 import {environments} from "../../../environments/environments";
-import { MatMenuModule} from '@angular/material/menu';
+import {MatMenuModule, MatMenuTrigger} from '@angular/material/menu';
+import {NgClass} from "@angular/common";
 
+interface Notification {
+  isRead: boolean;
+  text: string;
+  _id: string;
+  auction: string;
+}
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbar, MatIcon, MatButton, RouterLink, MatMenuModule],
+  imports: [MatToolbar, MatIcon, MatButton, RouterLink, MatMenuModule, MatMenuTrigger, MatIconButton, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
@@ -24,16 +31,18 @@ export class HeaderComponent implements OnInit, OnDestroy {
   protected userType: string | null = this.localStorage.get('userType');
   protected isUserLoggedIn: string | null = this.localStorage.get('isUserLoggedIn');
   protected changes: Subscription = new Subscription();
-  protected notifications: Array<{
-    text: string;
-    _id: string;
-     }> = []; // Array to hold notification messages
+  private menuTimeout: any;  // Timer for closing the menu
+  protected notifications: Array<Notification> = []; // Array to hold notification messages
+  protected notificationsCnt: number = 0; // Count of unread notifications
+
 
   constructor(
     protected localStorage: LocalStorageService,
     protected eventManager: EventManagerService,
-    protected socketService: SocketService
-  ) {}
+    protected socketService: SocketService,
+    private router: Router,
+  ) {
+  }
 
   ngOnInit(): void {
     this.changes.add(
@@ -43,9 +52,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
         this.isUserLoggedIn = this.localStorage.get('isUserLoggedIn');
         this.initSocket();
         this.socketService.receiveMessage((message) => {
-          if (message && message.length > 0) {
-            this.notifications = message; // Store notifications
-          }
+          this.setNotificationCnt(message);
         });
       }),
     );
@@ -61,15 +68,77 @@ export class HeaderComponent implements OnInit, OnDestroy {
     if (this.isUserLoggedIn) {
       this.initSocket();
       this.socketService.receiveMessage((message) => {
-        if (message && message.length > 0) {
-          this.notifications = message; // Store notifications
-        }
+        this.setNotificationCnt(message);
       });
     }
   }
 
+  private setNotificationCnt(notification: Notification[]) {
+    console.log('Notification received', notification);
+    if (notification && notification.length > 0) {
+      this.notifications = notification; // Store notifications
+
+      //count notification with isRead=false
+      this.notificationsCnt = notification.filter((notification) => !notification.isRead).length;
+    }
+  }
+
+  openMenu(menu: MatMenuTrigger) {
+    clearTimeout(this.menuTimeout);
+    menu.openMenu();
+  }
+
+  keepMenuOpen() {
+    clearTimeout(this.menuTimeout);
+  }
+
+  closeMenu(menu: MatMenuTrigger) {
+    this.menuTimeout = setTimeout(() => {
+      menu.closeMenu();
+    }, 300); // Adjust the timeout as needed
+  }
+
   ngOnDestroy(): void {
     this.changes.unsubscribe();
+  }
+
+  async viewAuction(auctionId: string) {
+    return await this.router.navigate(['/' + auctionId]); //FIXME: does not change page when I'm in an auction detail page
+  }
+
+
+
+
+  markNotificationAsSeen(notificationId: string) {
+    if (notificationId) {
+      let notification = this.notifications.find(notification => notification._id === notificationId);
+      if (notification && !notification.isRead) {
+        axios.patch(environments.BACKEND_URL + '/api/notifications/' + notificationId).then(res => {
+          if (res.status === 200) {
+            this.notificationsCnt--;
+          }
+        }).catch(err => {
+          console.error(err);
+        })
+      }
+    }
+  }
+
+  deleteNotification(notificationId: string) {
+    if (notificationId) {
+      let notification = this.notifications.find(notification => notification._id === notificationId);
+      if (notification) {
+        axios.delete(environments.BACKEND_URL + '/api/notifications/' + notificationId).then(res => {
+          if (res.status === 200) {
+            this.notificationsCnt--;
+            //remove notification from the list
+            this.notifications = this.notifications.filter(notification => notification._id !== notificationId);
+          }
+        }).catch(err => {
+          console.error(err);
+        })
+      }
+    }
   }
 
   initSocket() {
