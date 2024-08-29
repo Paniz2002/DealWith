243f0931b2a3a3bbfcab7816d9f7620c8578ae79
Paniz2002 <892678@stud.unive.it>
2024-08-29 11:53:26 +0200

    fixed typo

--- taw-backend/src/routes/auth.ts
+++ taw-backend/src/routes/auth.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { loginController } from "../controllers/login";
 import { registerController } from "../controllers/register";
 import { updateProfileController } from "../controllers/profile";
-import checkAuthenticationMiddleware from "../middlewares/auth";
+import authMiddleware from "../middlewares/auth";
 import { profileController } from "../controllers/logged-user";
 import { logoutController } from "../controllers/logout";
 const authRoutes: Router = Router();
@@ -10,10 +10,10 @@ const authRoutes: Router = Router();
 authRoutes.post("/login", loginController);
 authRoutes.post("/register", registerController);
 authRoutes.post("/logout", logoutController);
-authRoutes.get("/me", [checkAuthenticationMiddleware], profileController);
+authRoutes.get("/me", [authMiddleware], profileController);
 authRoutes.patch(
   "/me",
-  [checkAuthenticationMiddleware],
+  [authMiddleware],
   updateProfileController,
 );
 export default authRoutes;
